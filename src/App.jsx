2ae42991b172abd65986a5d9796dc1c87b26787f
Juan Duque <juanmanuelduque0920@gmail.com>
2022-07-18 18:04:45 -0500

    Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Carousel from './components/Carousel/Carousel';
 
 function App() {
   return (
     <Suspense fallback='Loading...'>
       <div>
         <Navbar />
+        <Carousel />
         {/* <Routes>
             <Route path="/" element={<Home/>} />
         </Routes> */}
--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,8 +1,25 @@
-import { useState } from "react";
+import { useEffect } from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+import s from './Carousel.module.css'
+import Autoplay from 'embla-carousel-autoplay'
 
 const Carousel = () => {
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    const [selectedBlock, setSelectedBlock] = useState(0);
+  const [ emblaRef, emblaApi ] = useEmblaCarousel( { loop: false }, [Autoplay()] )
+
+    useEffect( () => {
+        if( emblaApi ) {
+        }
+    }, [ emblaApi ] )
+
+  return (
+    <div className={ s.embla } ref={emblaRef}>
+      <div className={ s.embla__container }>
+        <div className={ s.embla__slide }>Slide 1</div>
+        <div className={ s.embla__slide }>Slide 2</div>
+        <div className={ s.embla__slide }>Slide 3</div>
+      </div>
+    </div>
+  )
 }
 
 export default Carousel;
