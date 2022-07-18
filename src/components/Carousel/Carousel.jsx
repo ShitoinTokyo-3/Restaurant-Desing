b30327f307b990774ead7084abf51b79bcb337f9
Juan Duque <juanmanuelduque0920@gmail.com>
2022-07-18 16:46:18 -0500

    Carpetas nuevas

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const Carousel = () => {
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [selectedBlock, setSelectedBlock] = useState(0);
+}
+
+export default Carousel;
