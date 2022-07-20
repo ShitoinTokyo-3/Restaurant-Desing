af4142ca9ae7893a52ee96d287aa8982c3170f1b
Angie <rest3media@gmail.com>
2022-07-19 21:03:28 -0500

    Imagenes subidas y texto arreglado

--- src/pages/Home/utilities/mediaCarousel/media.js
+++ src/pages/Home/utilities/mediaCarousel/media.js
@@ -1,6 +1,14 @@
-import Camisas from "../../../../assets/MockupCamisetas.png";
+import media_01 from "../../../../assets/media_01.png"
+import media_02 from "../../../../assets/media_02.png"
+import media_03 from "../../../../assets/media_03.png"
+import media_04 from "../../../../assets/media_04.png"
+import media_05 from "../../../../assets/media_05.png"
+import media_06 from "../../../../assets/media_06.png"
+import media_07 from "../../../../assets/media_07.png"
+import media_08 from "../../../../assets/media_08.png"
+import media_09 from "../../../../assets/media_09.png"
 
-export const media = [Camisas, Camisas, Camisas,Camisas, Camisas, Camisas,Camisas, Camisas, Camisas];
+export const media = [ media_01, media_02, media_03, media_04, media_05, media_06, media_07, media_08, media_09 ];
 export const mediaByIndex = index => media[index % media.length];
 
 
