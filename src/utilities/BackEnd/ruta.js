db569e8a15b6430c3db7bc4a1528d12ad1112302
Angie <rest3media@gmail.com>
2022-11-25 14:56:13 -0500

    Subiendo nueva ruta del backend 2

--- src/utilities/BackEnd/ruta.js
+++ src/utilities/BackEnd/ruta.js
@@ -1,5 +1,5 @@
-// const URL_BACKEND = "https://restaurant-desing-api-production-b9db.up.railway.app/";
+// const URL_BACKEND = "https://restaurant-desing-api-production-b9db.up.railway.app";
 // const URL_BACKEND = "http://localhost:3001";
-const URL_BACKEND = "https://restaurant-desing-api-production-b9db.up.railway.app/";
+const URL_BACKEND = "https://restaurant-desing-api-production-b9db.up.railway.app";
 
 export default URL_BACKEND;
